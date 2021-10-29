6f02e65e0bdaa1a2eb0769b29919bb4205b99331
Airels <33286314+Airels@users.noreply.github.com>
2021-10-29 19:46:00 +0200

    Implemented HUDElement

--- src/main/java/fr/r1r0r0/deltaengine/model/elements/HUDElement.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/elements/HUDElement.java
@@ -4,29 +4,38 @@ import fr.r1r0r0.deltaengine.model.Coordinates;
 import fr.r1r0r0.deltaengine.model.elements.Element;
 import fr.r1r0r0.deltaengine.model.sprites.Sprite;
 
+/**
+ * A graphical element for user. Can be used for HP bar or score text for example.
+ * HUDElement is completely detached from current level or anything else
+ */
 public class HUDElement implements Element {
+
+    private Sprite sprite;
+    private Coordinates coordinates;
+    private String name;
+
     @Override
     public Sprite getSprite() {
-        return null;
+        return sprite;
     }
 
     @Override
     public void setSprite(Sprite sprite) {
-
+        this.sprite = sprite;
     }
 
     @Override
     public Coordinates getCoordinates() {
-        return null;
+        return coordinates;
     }
 
     @Override
     public void setCoordinates(Coordinates coordinates) {
-
+        this.coordinates = coordinates;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 }
