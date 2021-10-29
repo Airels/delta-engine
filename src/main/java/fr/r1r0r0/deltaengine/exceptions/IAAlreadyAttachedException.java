cf17b37f4f3cfbf30a2afae4575b4578c342f0fe
Airels <33286314+Airels@users.noreply.github.com>
2021-10-29 19:40:01 +0200

    Implemented Entity

---
+++ src/main/java/fr/r1r0r0/deltaengine/exceptions/IAAlreadyAttachedException.java
@@ -0,0 +1,14 @@
+package fr.r1r0r0.deltaengine.exceptions;
+
+import fr.r1r0r0.deltaengine.model.elements.Entity;
+
+public class IAAlreadyAttachedException extends Exception {
+
+    public IAAlreadyAttachedException(String msg) {
+        super(msg);
+    }
+
+    public IAAlreadyAttachedException(Entity entity) {
+        this(entity.getName() + " already have attached IA");
+    }
+}
