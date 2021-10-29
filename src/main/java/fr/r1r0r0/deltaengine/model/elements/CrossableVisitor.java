04ca7286258778c1aa41fe922029edbf793ea351
Airels <33286314+Airels@users.noreply.github.com>
2021-10-29 16:45:37 +0200

    Implemented CrossableVisitor

--- src/main/java/fr/r1r0r0/deltaengine/model/elements/CrossableVisitor.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/elements/CrossableVisitor.java
@@ -1,7 +1,16 @@
 package fr.r1r0r0.deltaengine.model.elements;
 
+/**
+ * A modified Visitor pattern, to access isCrossable method by the engine.
+ * Used to avoid to override isCrossable method in Case (because it is overriden by UncrossableCase)
+ */
 public class CrossableVisitor {
 
+    /**
+     * Check if a given case is crossable (equivalent to "c instanceof Case")
+     * @param c case to check
+     * @return true if case is crossable, false otherwise
+     */
     public static boolean isCaseCrossable(Case c) {
         return c.isCrossable();
     }
