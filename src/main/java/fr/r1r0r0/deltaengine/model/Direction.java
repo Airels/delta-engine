8c920107d8a7910df0f9adc367b8056ff2c01d42
Airels <33286314+Airels@users.noreply.github.com>
2021-10-29 17:00:02 +0200

    Javadoc Direction

--- src/main/java/fr/r1r0r0/deltaengine/model/Direction.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/Direction.java
@@ -1,8 +1,12 @@
 package fr.r1r0r0.deltaengine.model;
 
+/**
+ * Current directions. IDLE means no movement.
+ */
 public enum Direction {
     LEFT,
     UP,
     RIGHT,
-    DOWN
+    DOWN,
+    IDLE
 }
