a7b72cdb5f89a55da51700987a491e1629327862
Airels <33286314+Airels@users.noreply.github.com>
2021-10-29 16:27:23 +0200

    Added javadoc

--- src/main/java/fr/r1r0r0/deltaengine/model/Coordinates.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/Coordinates.java
@@ -1,17 +1,33 @@
 package fr.r1r0r0.deltaengine.model;
 
+/**
+ * Coordinates of a point, or object, coded by two doubles for 2D position.
+ */
 public class Coordinates {
     private double x, y;
-    
+
+    /**
+     * Default constructor. Creates a Coordinates given x and y positions.
+     * @param x horizontal position
+     * @param y vertical position
+     */
     public Coordinates(double x, double y) {
         this.x = x;
         this.y = y;
     }
-    
+
+    /**
+     * Get saved x position.
+     * @return x double current x of an element
+     */
     public double getX() {
         return x;
     }
-    
+
+    /**
+     * Get saved y position.
+     * @return y double current y of an element
+     */
     public double getY() {
         return y;
     }
