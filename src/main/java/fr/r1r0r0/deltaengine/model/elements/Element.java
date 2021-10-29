13d2cc40a9cfd08f587c21b63e9e86d25618a9fa
Airels <33286314+Airels@users.noreply.github.com>
2021-10-29 16:34:01 +0200

    Added Element javadoc

--- src/main/java/fr/r1r0r0/deltaengine/model/elements/Element.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/elements/Element.java
@@ -3,11 +3,39 @@ package fr.r1r0r0.deltaengine.model.elements;
 import fr.r1r0r0.deltaengine.model.Coordinates;
 import fr.r1r0r0.deltaengine.model.sprites.Sprite;
 
+/**
+ * A graphical element. Could be a Case, an Entity or anything graphical.
+ * Possess coordinates, a Sprite and a Name.
+ */
 public interface Element {
 
+    /**
+     * Returns sprite of the element
+     * @return Sprite of the element
+     */
     Sprite getSprite();
+
+    /**
+     * Set sprite of an element
+     * @param sprite sprite to set
+     */
     void setSprite(Sprite sprite);
+
+    /**
+     * Coordinates of the element in the game.
+     * @return Coordinates of the element
+     */
     Coordinates getCoordinates();
+
+    /**
+     * Allows setting new coordinates for the element.
+     * @param coordinates new coordinates
+     */
     void setCoordinates(Coordinates coordinates);
+
+    /**
+     * Name getter of the element
+     * @return String name of the element
+     */
     String getName();
 }
