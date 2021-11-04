d203dde5a4653d0f88aa807fc1b72668055a41fc
Airels <33286314+Airels@users.noreply.github.com>
2021-11-04 21:26:38 +0100

    Implemented special event

---
+++ src/main/java/fr/r1r0r0/deltaengine/model/events/KeyEvent.java
@@ -0,0 +1,34 @@
+package fr.r1r0r0.deltaengine.model.events;
+
+/**
+ * Special event used to bind actions on Keyboard events.
+ * Can bind two triggers called when a specific key is pressed or released.
+ */
+public final class KeyEvent {
+
+    private final Trigger pressTrigger, releaseTrigger;
+
+    /**
+     * Default constructor. Defines which triggers will be called when key event is caught by the engine.
+     * @param pressTrigger trigger when key is pressed
+     * @param releaseTrigger trigger when key is released
+     */
+    public KeyEvent(Trigger pressTrigger, Trigger releaseTrigger) {
+        this.pressTrigger = pressTrigger;
+        this.releaseTrigger = releaseTrigger;
+    }
+
+    /**
+     * Run bound trigger containing actions to execute when a key is pressed
+     */
+    public void runPressTrigger() {
+        pressTrigger.trigger();
+    }
+
+    /**
+     * Run bound trigger containing actions to execute when a key is released
+     */
+    public void runReleaseTrigger() {
+        releaseTrigger.trigger();
+    }
+}
