43a1eb5af56c7ee2fd46a9e3d6aa12ce38329441
Valorator <vidal.guillaume.1@etu.univ-amu.fr>
2021-11-06 16:44:30 +0100

    comment class PhysicsEngine

--- src/main/java/fr/r1r0r0/deltaengine/model/engines/PhysicsEngine.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/engines/PhysicsEngine.java
@@ -7,21 +7,33 @@ import fr.r1r0r0.deltaengine.model.elements.Case;
 import fr.r1r0r0.deltaengine.model.elements.CrossableVisitor;
 import fr.r1r0r0.deltaengine.model.elements.Entity;
 
+/**
+ * A physical engine
+ */
 class PhysicsEngine implements Engine {
 
     private Map map;
     private long previousRunTime;
 
+    /**
+     * Constructor
+     */
     public PhysicsEngine () {
         map = null;
         previousRunTime = System.currentTimeMillis();
     }
 
+    /**
+     * Initializer
+     */
     @Override
     public void init() {
 
     }
 
+    /**
+     * Update the coordinate of all entity in the current map
+     */
     @Override
     public void run() {
         long currentRunTime = System.currentTimeMillis();
@@ -30,28 +42,45 @@ class PhysicsEngine implements Engine {
         if (map != null) {
             for (Entity entity : map.getEntities()) {
                 updateCoordinate(entity,deltaTime);
-                entity.setDirection(Direction.IDLE);
             }
         }
     }
 
+    /**
+     * Update the coordinate of an entity
+     * @param entity an entity
+     * @param deltaTime a interval of time used to calc the movement
+     */
     private void updateCoordinate (Entity entity, long deltaTime) {
         Coordinates nextCoordinate = entity.getCoordinates().getNextCoordinates(entity.getDirection(),
                 entity.getSpeed()*deltaTime);
         if (isValidCoordinates(nextCoordinate)) entity.setCoordinates(nextCoordinate);
+        else entity.setDirection(Direction.IDLE);
     }
 
+    /**
+     * Returns if the coordinate is a valid coordinate in the map
+     * @param coordinates a coordinate
+     * @return if the coordinate is a valid coordinate in the map
+     */
     private boolean isValidCoordinates (Coordinates coordinates) {
-        int x = (coordinates.getX() >= 0) ? (int) coordinates.getX() : ((int) coordinates.getX() + 1);
-        int y = (coordinates.getY() >= 0) ? (int) coordinates.getY() : ((int) coordinates.getY() + 1);
+        int x = (coordinates.getX() >= 0) ? (int) coordinates.getX() : ((int) coordinates.getX() - 1);
+        int y = (coordinates.getY() >= 0) ? (int) coordinates.getY() : ((int) coordinates.getY() - 1);
         Case nextCase = map.getCase(x,y);
         return nextCase != null && CrossableVisitor.isCaseCrossable(nextCase);
     }
 
+    /**
+     * Replace the current map with another
+     * @param map a map
+     */
     public void setMap(Map map) {
         this.map = map;
     }
 
+    /**
+     * Clear the current map
+     */
     public void clearMap() {
         map = null;
     }
