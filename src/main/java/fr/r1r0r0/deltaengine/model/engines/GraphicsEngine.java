1bb40d195ff0ed4471d9f3665fcb37df74b128ea
tom.david-broglio <tom.david-broglio@etu.univ-amu.fr>
2021-11-08 00:00:17 +0100

    un peu de doc...

--- src/main/java/fr/r1r0r0/deltaengine/model/engines/GraphicsEngine.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/engines/GraphicsEngine.java
@@ -4,6 +4,7 @@ import fr.r1r0r0.deltaengine.model.Coordinates;
 import fr.r1r0r0.deltaengine.model.elements.Case;
 import fr.r1r0r0.deltaengine.model.elements.Element;
 import fr.r1r0r0.deltaengine.model.elements.Entity;
+import fr.r1r0r0.deltaengine.model.elements.HUDElement;
 import fr.r1r0r0.deltaengine.model.sprites.Sprite;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -14,6 +15,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * Graphic engine takes care of maintaining the view updated
+ */
 class GraphicsEngine implements Engine {
 
     private ArrayList<Element> elements;
@@ -22,6 +26,20 @@ class GraphicsEngine implements Engine {
     private Scene scene;
     private double z;
 
+    private boolean initialized;
+    private boolean started;
+
+    /**
+     * Constructor to instantiate the engine but not initialising its components
+     */
+    public GraphicsEngine(){
+        initialized = false;
+        started = false;
+    }
+
+    /**
+     * initialising the graphic engine components
+     */
     @Override
     public void init() {
         elements = new ArrayList<>();
@@ -33,16 +51,29 @@ class GraphicsEngine implements Engine {
 
         stage = new Stage();
         stage.setScene(scene);
+
+        initialized = true;
     }
 
+    /**
+     * Graphic engine loop
+     */
     @Override
     public void run() {
+        if (started)
+            throw new RuntimeException("Graphic Engine is already running");
+        started = true;
         for (Element e:elements){
             updateElement(e);
         }
+        started = false;
 
     }
 
+    /**
+     * Update an element's graphical view
+     * @param e the element to be updated
+     */
     private void updateElement(Element e){
         if(!elements.contains(e)) throw new NoSuchElementException();
 
@@ -59,27 +90,49 @@ class GraphicsEngine implements Engine {
         else if(n.isVisible())n.setVisible(false);
     }
 
+    /**
+     * set up the cases matrix
+     * @param cases to set up
+     * @param width ?? TODO
+     * @param height ??
+     */
     public void setCases(List<Case> cases, int width, int height) {
+        for (Element e:cases){
+            //TODO mettre la bonne taille des cases (width != height ?)
+            e.getSprite().getNode().resize(1,1);
+            addElement(e);
+        }
         stage.setWidth(width);
         stage.setHeight(height);
     }
 
+    /**
+     * add an element to display
+     * @param element element to display
+     */
     public void addElement(Element element) {
         if (!elements.contains(element)){
             elements.add(element);
             pane.getChildren().add(element.getSprite().getNode());
+            if (element.getClass() == HUDElement.class) element.getSprite().getNode().setViewOrder(-1.0);
+            updateElement(element);
         }
     }
 
+    /**
+     * remove an element from the graphic engine and from being displayed
+     * @param element an element from the graphic engine
+     */
     public void removeElement(Element element) {
         elements.remove(element);
         pane.getChildren().remove(element.getSprite().getNode());
-
     }
 
+    /**
+     * Empty the all element from the graphic engine and from the view
+     */
     public void clearElements() {
         elements.clear();
         pane.getChildren().clear();
-
     }
 }
