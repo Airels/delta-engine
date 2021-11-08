f3354b9389bab04f11c9e0d16d5a36dc108e8096
Airels <yohan13240@hotmail.fr>
2021-11-08 14:46:43 +0100

    Added keys

--- src/main/java/fr/r1r0r0/deltaengine/model/engines/utils/Key.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/engines/utils/Key.java
@@ -3,5 +3,8 @@ package fr.r1r0r0.deltaengine.model.engines.utils;
 public enum Key {
     A, Z, E, R, T, Y, U, I, O, P, Q, S, D, F, G, H, J, K, L, M, W, X, C, V, B, N,
     ARROW_UP, ARROW_LEFT, ARROW_DOWN, ARROW_RIGHT,
-    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, ZERO
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, ZERO,
+    ENTER, BACK_SPACE, ESCAPE,
+    LEFT_CTRL, RIGHT_CTRL, LEFT_SHIFT, RIGHT_SHIFT,
+    ALT, ALT_GR, TAB
 }
