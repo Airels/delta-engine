276cc541ee0c77cb75380f77b556fc4dd0d853dd
Airels <yohan13240@hotmail.fr>
2021-11-15 15:27:58 +0100

    add commentary to the class BufferedHashMap, yolo !

--- src/main/java/fr/r1r0r0/deltaengine/model/BufferedHashMap.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/BufferedHashMap.java
@@ -3,9 +3,9 @@ package fr.r1r0r0.deltaengine.model;
 import java.util.*;
 
 /**
- * TODO
- * @param <E>
- * @param <F>
+ * A custom HashMap that save element that was added and remove
+ * @param <E> the type of the key
+ * @param <F> the type of the value
  */
 public class BufferedHashMap<E,F> extends HashMap<E,F> {
 
@@ -13,18 +13,20 @@ public class BufferedHashMap<E,F> extends HashMap<E,F> {
     private final Collection<F> previousRemove;
 
     /**
-     * TODO
+     * Constructor
      */
     public BufferedHashMap() {
+        super();
         previousAdd = new Stack<>();
         previousRemove = new Stack<>();
     }
 
     /**
-     * TODO
-     * @param key
-     * @param value
-     * @return
+     * Add a couple key/value in the hashmap
+     * Save the value in the collection of the attribute previousAdd
+     * @param key a key
+     * @param value a value
+     * @return the previous value of the key
      */
     @Override
     public F put(E key, F value) {
@@ -33,9 +35,10 @@ public class BufferedHashMap<E,F> extends HashMap<E,F> {
     }
 
     /**
-     * TODO
-     * @param key
-     * @return
+     * Remove a key in the hashmap
+     * Save the value in the collection of the attribute previousRemove
+     * @param key a key
+     * @return the value matching to the key that was removed
      */
     @Override
     public F remove(Object key) {
@@ -45,7 +48,7 @@ public class BufferedHashMap<E,F> extends HashMap<E,F> {
     }
 
     /**
-     * TODO
+     * Clear the hashmap
      */
     @Override
     public void clear() {
@@ -53,30 +56,30 @@ public class BufferedHashMap<E,F> extends HashMap<E,F> {
     }
 
     /**
-     * TODO
-     * @return
+     * Getter of the attribute previousAdd
+     * @return the attribute previousAdd
      */
     public Collection<F> getPreviousAdd () {
         return previousAdd;
     }
 
     /**
-     * TODO
-     * @return
+     * Getter of the attribute previousRemove
+     * @return the attribute previousRemove
      */
     public Collection<F> getPreviousRemove () {
         return previousRemove;
     }
 
     /**
-     * TODO
+     * Clear the attribute previousAdd
      */
     public void clearPreviousAdd () {
         previousAdd.clear();
     }
 
     /**
-     * TODO
+     * Clear the attribute previousRemove
      */
     public void clearPreviousRemove () {
         previousRemove.clear();
