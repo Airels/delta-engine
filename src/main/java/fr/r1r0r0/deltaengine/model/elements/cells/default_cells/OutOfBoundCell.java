b49343cd4c0320cda45129ae55d769848d8db914
Airels <33286314+Airels@users.noreply.github.com>
2021-11-24 19:39:45 +0100

    Updated javadoc

--- src/main/java/fr/r1r0r0/deltaengine/model/elements/cells/default_cells/OutOfBoundCell.java
+++ src/main/java/fr/r1r0r0/deltaengine/model/elements/cells/default_cells/OutOfBoundCell.java
@@ -5,7 +5,7 @@ import fr.r1r0r0.deltaengine.model.sprites.shapes.Rectangle;
 import fr.r1r0r0.deltaengine.view.colors.Color;
 
 /**
- * A VoidCell black case. Just to represent nothing.
+ * A cell to represents out of bounds cells. Used to avoid null values.
  */
 public class OutOfBoundCell extends UncrossableCell {
 
