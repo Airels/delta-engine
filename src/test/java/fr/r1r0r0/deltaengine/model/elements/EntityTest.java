df0608b3e0d81c576c42bf8861700ef0b620d83d
GrosFlan <guillaume13.nicolai@gmail.com>
2021-12-01 10:58:05 +0100

    tests

--- src/test/java/fr/r1r0r0/deltaengine/model/elements/EntityTest.java
+++ src/test/java/fr/r1r0r0/deltaengine/model/elements/EntityTest.java
@@ -1,40 +1,21 @@
 package fr.r1r0r0.deltaengine.model.elements;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import fr.r1r0r0.deltaengine.model.Coordinates;
 import fr.r1r0r0.deltaengine.model.Dimension;
 import fr.r1r0r0.deltaengine.model.elements.entity.Entity;
-import fr.r1r0r0.deltaengine.model.sprites.Sprite;
-import javafx.scene.Node;
-import javafx.scene.shape.Rectangle;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import fr.r1r0r0.deltaengine.model.sprites.InvisibleSprite;
 
 class EntityTest {
 
     @Test
     void testCollide() {
-        Entity entity1 = new Entity(null, new Coordinates(0, 0), new Sprite() {
-            @Override
-            public Node getNode() {
-                return new Rectangle();
-            }
-
-            @Override
-            public void resize(double width, double height) {
-
-            }
-        }, new Dimension(1, 1));
-        Entity entity2 = new Entity(null, new Coordinates(10, 10), new Sprite() {
-            @Override
-            public Node getNode() {
-                return new Rectangle();
-            }
-
-            @Override
-            public void resize(double width, double height) {
-
-            }
-        }, new Dimension(1, 1));
+        Entity entity1 = new Entity(null, new Coordinates(0., 0.),
+                InvisibleSprite.getInstance(), new Dimension(1, 1));
+        Entity entity2 = new Entity(null, new Coordinates(10.0, 10.0),
+                InvisibleSprite.getInstance(), new Dimension(1, 1));
         Assertions.assertTrue(entity1.testCollide(entity1));
         Assertions.assertFalse(entity1.testCollide(entity2));
 
